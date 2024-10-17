3d4583832aed6c3e624691fcadb9b3a351599450
Alexandr Cherkasov <alexdsad9@yandex.ru>
2024-10-17 20:45:25 +0300

    Реализована кастомизация страницы блога через открывающуюся панель

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,19 +1,32 @@
 import arrow from 'src/images/arrow.svg';
-
+import clsx from 'clsx';
 import styles from './ArrowButton.module.scss';
 
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+type Props = {
+	visibility: boolean;
+	onClick: OnClick;
+};
+
+export const ArrowButton = ({ visibility, onClick }: Props) => {
 	return (
-		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
+		/* Не забываем указывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={clsx(
+				styles.container,
+				visibility ? styles.container_open : ''
+			)}
+			onClick={onClick}>
+			<img
+				src={arrow}
+				alt='иконка стрелочки'
+				className={clsx(styles.arrow, visibility ? styles.arrow_open : '')}
+			/>
 		</div>
 	);
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,49 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import {
+	defaultArticleState,
+	ArticleStateType,
+} from './constants/articleProps';
 
-import './styles/index.scss';
 import styles from './styles/index.module.scss';
+import './styles/index.scss';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	// Состояние для хранения текущих параметров статьи
+	const [articleState, setArticleState] =
+		useState<ArticleStateType>(defaultArticleState);
+
+	// Обработчик применения новых параметров
+	const handleApply = (newState: ArticleStateType) => {
+		setArticleState(newState);
+	};
+
+	// Обработчик сброса параметров к значениям по умолчанию
+	const handleReset = () => {
+		setArticleState(defaultArticleState);
+	};
+
 	return (
 		<div
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					// Применение параметров через CSS-переменные
+					'--font-family': articleState.fontFamilyOption.value,
+					'--font-size': articleState.fontSizeOption.value,
+					'--font-color': articleState.fontColor.value,
+					'--container-width': articleState.contentWidth.value,
+					'--bg-color': articleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm stateSubmit={handleApply} stateReset={handleReset} />
 			<Article />
 		</div>
 	);
